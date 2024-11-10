471ec2c7170d31ed1b3000d3d94423d9c42013bf
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-11-10 18:52:45 +0000

    update

--- src/lib/contentImage.ts
+++ src/lib/contentImage.ts
@@ -1,35 +1,35 @@
-export function parseContentfulContentImage(asset) {
-  if (!asset) {
-    return null;
-  }
+// export function parseContentfulContentImage(asset) {
+//   if (!asset) {
+//     return null;
+//   }
 
-  if (!("fields" in asset)) {
-    return null;
-  }
+//   if (!("fields" in asset)) {
+//     return null;
+//   }
 
-  return {
-    src: `https:${asset.fields.file?.url}` || "",
-    alt: asset.fields.description || "",
-    width: asset.fields.file?.details.image?.width || 0,
-    height: asset.fields.file?.details.image?.height || 0,
-  };
-}
+//   return {
+//     src: `https:${asset.fields.file?.url}` || "",
+//     alt: asset.fields.description || "",
+//     width: asset.fields.file?.details.image?.width || 0,
+//     height: asset.fields.file?.details.image?.height || 0,
+//   };
+// }
 
-export function parseContentfulContentImages(assets) {
-  if (!Array.isArray(assets)) {
-    return [];
-  }
+// export function parseContentfulContentImages(assets) {
+//   if (!Array.isArray(assets)) {
+//     return [];
+//   }
 
-  return assets.map((asset) => {
-    if (!asset || !("fields" in asset)) {
-      return null;
-    }
+//   return assets.map((asset) => {
+//     if (!asset || !("fields" in asset)) {
+//       return null;
+//     }
 
-    return {
-      src: `https:${asset.fields.file?.url}` || "",
-      alt: asset.fields.description || "",
-      width: asset.fields.file?.details.image?.width || 0,
-      height: asset.fields.file?.details.image?.height || 0,
-    };
-  });
-}
+//     return {
+//       src: `https:${asset.fields.file?.url}` || "",
+//       alt: asset.fields.description || "",
+//       width: asset.fields.file?.details.image?.width || 0,
+//       height: asset.fields.file?.details.image?.height || 0,
+//     };
+//   });
+// }
